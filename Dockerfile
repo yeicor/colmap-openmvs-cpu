3349385ac19fd4c3cb71af69676d44948444760c
Yeicor <4929005+Yeicor@users.noreply.github.com>
2025-03-17 23:29:42 +0100

    Add initial untested dockerfile

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM ubuntu:plucky
+
+# Install system dependencies (build and runtime)
+RUN apt-get update -yq && apt-get -yq install build-essential git cmake libpng-dev libjpeg-dev libtiff-dev libglu1-mesa-dev libglew-dev libglfw3-dev python3-dev git cmake ninja-build build-essential libboost-program-options-dev libboost-graph-dev libboost-system-dev libeigen3-dev libflann-dev libfreeimage-dev libmetis-dev libgoogle-glog-dev libgtest-dev libgmock-dev libsqlite3-dev libglew-dev qtbase5-dev libqt5opengl5-dev libcgal-dev libceres-dev libboost-iostreams-dev libboost-program-options-dev libboost-system-dev libboost-serialization-dev libopencv-dev libcgal-dev libcgal-qt6-dev
+WORKDIR /build
+
+# Copy and build colmap git submodule
+COPY colmap colmap
+RUN cd colmap && mkdir build && cd build && cmake .. -GNinja && ninja && ninja install
+
+# Copy and build openmvs git submodule
+COPY openmvs openmvs
+RUN cd openmvs && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DVCG_ROOT=./vcglib .. && cmake --build . -j$(nproc) && cmake --install .
+ENV PATH /usr/local/bin/OpenMVS:$PATH
+
+# Create and switch to a custom user
+ARG UID 1000
+ARG GID 1000
+RUN groupadd -g $GID group && \
+    useradd -m -u $UID -g group user
+USER user
+WORKDIR /home/user
+
+# Set entrypoint
+COPY entrypoint.sh entrypoint.sh
+ENTRYPOINT entrypoint.sh
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+cd "$(dirname $(realpath $0))"
+set -ex
+
+export obj="$1"
+if [ -z "$obj" ]; then
+    echo "missing obj folder as the first argument. Should point to a folder with an images/ subfolder"
+    exit 1
+fi
+
+cd "$obj"
+if [ ! -d "colmap/dense/0" ] || [ ! -z "$force_colmap" ]; then
+    mkdir -p "colmap"
+    colmap automatic_reconstructor --image_path "../images" --workspace_path . --quality extreme --camera_model OPENCV --single_camera=1 --use_gpu=0 || true
+    if [ ! -d "colmap/dense/0" ]; then
+        echo "Colmap failed to create at least one dense reconstruction folder"
+        exit 1
+    fi
+fi
+
+if [ ! -f "openmvs/scene_mesh.ply" ] || [ ! -z "$force_openmvs_scene_mesh" ]; then
+    mkdir -p "openmvs"
+    InterfaceCOLMAP -i ../colmap/dense/0 -o scene.mvs && ReconstructMesh scene.mvs
+fi
+
+# Optional manual crop step (open scene_mesh.ply in blender and remove out-of-bounds triangles)
+
+if [ ! -f "openmvs/scene_mesh_refined_textured.ply" ] || [ ! -z "$force_openmvs_scene_mesh_refined_textured" ]; then
+    RefineMesh -i scene.mvs -m scene_mesh.ply -o scene_mesh_refined.ply && TextureMesh -i scene.mvs -m scene_mesh_refined.ply -o scene_mesh_refined_textured.obj
+fi
+    
+if [ ! -f "openmvs/scene_dense_mesh.ply" ] || [ ! -z "$force_openmvs_scene_dense_mesh" ]; then
+    DensifyPointCloud scene.mvs && ReconstructMesh scene_dense.mvs
+fi
+
+# Optional manual crop step (open scene_dense_mesh.ply in blender and remove out-of-bounds triangles)
+
+if [ ! -f "openmvs/scene_dense_mesh_refined.ply" ] || [ ! -z "$force_openmvs_scene_dense_mesh_refined" ]; then
+    RefineMesh -i scene_dense.mvs -m scene_dense_mesh.ply -o scene_dense_mesh_refined.ply && TextureMesh -i scene_dense.mvs -m scene_dense_mesh_refined.ply -o scene_dense_mesh_refined_textured.obj
+fi
+
+echo "Finished succesfully!"
+
