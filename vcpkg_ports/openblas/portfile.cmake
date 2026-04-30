06bfdfbd1b965b65857566e27bd19303a44ba397
Yeicor <yeicor+no-reply@github.com>
2026-04-30 10:31:27 +0200

    Fix openblas for arm64

--- vcpkg_ports/openblas/portfile.cmake
+++ vcpkg_ports/openblas/portfile.cmake
@@ -2,7 +2,14 @@
 file(COPY "${VCPKG_ROOT_DIR}/ports/openblas/" DESTINATION "${CMAKE_CURRENT_LIST_DIR}" PATTERN "portfile.cmake" EXCLUDE)
 
 # Patch upstream portfile.cmake (support and optimize for more platforms, not just native at build time)
-string(REPLACE "-DBUILD_TESTING=OFF" "-DBUILD_TESTING=OFF -DDYNAMIC_ARCH=ON -DDYNAMIC_OLDER=ON" upstream_content "${upstream_content}")
+# https://github.com/OpenMathLib/OpenBLAS#support-for-multiple-targets-in-a-single-library
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "^x86_64")
+  string(REPLACE "-DBUILD_TESTING=OFF" "-DBUILD_TESTING=OFF -DDYNAMIC_ARCH=ON -DDYNAMIC_OLDER=ON -DTARGET=HASWELL" upstream_content "${upstream_content}")
+elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "^aarch64")
+  string(REPLACE "-DBUILD_TESTING=OFF" "-DBUILD_TESTING=OFF -DDYNAMIC_ARCH=ON -DDYNAMIC_OLDER=ON -DTARGET=ARMV8" upstream_content "${upstream_content}")
+else()
+  message(FATAL_ERROR "Unsupported architecture ${CMAKE_SYSTEM_PROCESSOR}")
+endif()
 file(WRITE "${CMAKE_CURRENT_LIST_DIR}/portfile_upstream_patched.cmake" "${upstream_content}")
 
 # Include the upstream portfile.cmake
